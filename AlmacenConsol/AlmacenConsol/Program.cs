e86eefed2381cd9092eea6210564cec941bcceb0
a2n0g0e2l <125489061+a2n0g0e2l@users.noreply.github.com>
2023-03-18 21:43:19 -0600

    re

---
+++ AlmacenConsol/AlmacenConsol/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AlmacenConsol.Models;
+
+Console.WriteLine("Hello, World!");
+
+AlmacenContext db = new AlmacenContext();
+
+Console.WriteLine("Ingrese el nombre del producto: ");
+string nombre = Console.ReadLine();
+
+Console.WriteLine("Ingrese la descripción del producto: ");
+string descripcion = Console.ReadLine();
+
+Console.WriteLine("Ingrese el precio del producto: ");
+decimal precio = decimal.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese el stock del producto: ");
+int stock = int.Parse(Console.ReadLine());
+
+
+var producto = new Producto { Nombre = nombre, Descripcion = descripcion, Precio = precio, Stock = stock };
+db.Productos.Add(producto);
+db.SaveChanges();
